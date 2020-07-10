d4ce99cb641f4958f8d785382c20552b176f894e
Peter O'Connor <sunnyflunk@gmail.com>
2020-07-10 12:24:23 +1000

    Import download functions from https://github.com/serpent-linux/bootstrap-scripts
    Slightly modified to suit differing paths

--- common/shared.sh
+++ common/shared.sh
@@ -37,3 +37,50 @@ function requireTools()
         which "${tool}" &>/dev/null  || serpentFail "Missing host executable: ${tool}"
     done
 }
+
+
+# Verify the download is correct
+function verifyDownload()
+{
+    [ ! -z "${1}" ] || serpentFail "Incorrect use of verifyDownload"
+    sourceFile="${executionPath}/sources/${1}"
+    [ -f "${sourceFile}" ] || serpentFail "Missing source file: ${sourceFile}"
+    sourceURL="$(cat ${sourceFile} | cut -d ' ' -f 1)"
+    sourceHash="$(cat ${sourceFile} | cut -d ' ' -f 2)"
+    [ ! -z "${sourceURL}" ] || serpentFail "Missing URL for source: $1"
+    [ ! -z "${sourceHash}" ] || serpentFail "Missing hash for source: $1"
+    sourcePathBase=$(basename "${sourceURL}")
+    sourcePath="${downloadCache}/${sourcePathBase}"
+
+    printInfo "Computing hash for ${sourcePathBase}"
+
+    computeHash=$(sha256sum "${sourcePath}" | cut -d ' ' -f 1)
+    [ $? -eq 0 ] || serpentFail "Failed to compute SHA256sum"
+
+    [ "${computeHash}" == "${sourceHash}" ] || serpentFail "Corrupt download: ${sourcePath}"
+}
+
+# Download a file from sources/
+function downloadSource()
+{
+    [ ! -z "${1}" ] || serpentFail "Incorrect use of downloadSource"
+    sourceFile="${executionPath}/sources/${1}"
+    [ -f "${sourceFile}" ] || serpentFail "Missing source file: ${sourceFile}"
+    sourceURL="$(cat ${sourceFile} | cut -d ' ' -f 1)"
+    sourceHash="$(cat ${sourceFile} | cut -d ' ' -f 2)"
+    [ ! -z "${sourceURL}" ] || serpentFail "Missing URL for source: $1"
+    [ ! -z "${sourceHash}" ] || serpentFail "Missing hash for source: $1"
+    sourcePathBase=$(basename "${sourceURL}")
+    sourcePath="${downloadCache}/${sourcePathBase}"
+
+    mkdir -p "${downloadCache}" || serpentFail "Failed to create download tree"
+
+    if [[ -f "${sourcePath}" ]]; then
+        printInfo "Skipping download of ${sourcePathBase}"
+        return
+    fi
+
+    printInfo "Downloading ${sourcePathBase}"
+    curl -L --output "${sourcePath}" "${sourceURL}"
+    verifyDownload "${1}"
+}
