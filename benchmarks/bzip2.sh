d8ae0fae8be40534fcd93787c182415bc72fd860
Peter O'Connor <sunnyflunk@gmail.com>
2020-08-07 12:34:09 +1000

    Update benchmark order

--- benchmarks/bzip2.sh
+++ benchmarks/bzip2.sh
@@ -5,14 +5,14 @@ benchmarkSetup=("cp ${BT_CACHE_DIR}/linux-4.0.tar.xz ."
                 "xz -d linux-4.0.tar.xz"
                 "mv linux-4.0.tar linux.tar")
 benchmarkLabels=("Compress Kernel (-3)"
-                 "Decompress Kernel"
-                 "Compress Kernel (-9)")
+                 "Compress Kernel (-9)"
+                 "Decompress Kernel")
 benchmarkTest=("bzip2 -z -k -f -3 linux.tar"
-               "bzip2 -d -k -f linux.tar.bz2"
-               "bzip2 -z -k -f -9 linux.tar")
+               "bzip2 -z -k -f -9 linux.tar"
+               "bzip2 -d -k -f linux.tar.bz2")
 benchmarkValidation=("wc -c linux.tar.bz2 | cut -d ' ' -f 1"
-                     "wc -c linux.tar | cut -d ' ' -f 1"
-                     "wc -c linux.tar.bz2 | cut -d ' ' -f 1")
+                     "wc -c linux.tar.bz2 | cut -d ' ' -f 1"
+                     "wc -c linux.tar | cut -d ' ' -f 1")
 benchmarkRepetition=(1
                      1
                      1)
--- benchmarks/zstd.sh
+++ benchmarks/zstd.sh
@@ -5,17 +5,17 @@ benchmarkSetup=("cp ${BT_CACHE_DIR}/linux-4.0.tar.xz ."
                 "xz -d linux-4.0.tar.xz"
                 "mv linux-4.0.tar linux.tar")
 benchmarkLabels=("Compress Kernel (-8 -T1)"
-                 "Decompress Kernel"
-                 "Compress Kernel (-19 -T4)")
+                 "Compress Kernel (-19 -T4)"
+                 "Decompress Kernel")
 benchmarkTest=("zstd -k -f -8 -T1 linux.tar -o linux.tar.zst"
-               "zstd -d -k -f linux.tar.zst -o linux.tar"
-               "zstd -k -f -19 -T4 linux.tar -o linux.tar.zst")
+               "zstd -k -f -19 -T4 linux.tar -o linux.tar.zst"
+               "zstd -d -k -f linux.tar.zst -o linux.tar")
 benchmarkValidation=("wc -c linux.tar.zst | cut -d ' ' -f 1"
-                     "wc -c linux.tar | cut -d ' ' -f 1"
-                     "wc -c linux.tar.zst | cut -d ' ' -f 1")
+                     "wc -c linux.tar.zst | cut -d ' ' -f 1"
+                     "wc -c linux.tar | cut -d ' ' -f 1")
 benchmarkRepetition=(1
-                     5
-                     1)
+                     1
+                     5)
 benchmarkAnalyze=( "${benchmarkTest[@]}" )
 
 # Import functions for this benchmark
