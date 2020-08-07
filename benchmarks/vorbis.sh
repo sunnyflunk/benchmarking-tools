7993540c44652659dbd8a7e42aa78ee29242306a
Peter O'Connor <sunnyflunk@gmail.com>
2020-08-07 15:10:28 +1000

    Add vorbis benchmark

---
+++ benchmarks/vorbis.sh
@@ -0,0 +1,21 @@
+#!/bin/true
+
+. ${executionPath}/benchmarks/helpers/wavfile.sh
+benchmarkLabels=("Encode (-b 128)"
+                 "Encode (-q 10)"
+                 "Decode")
+benchmarkTest=("oggenc -b 128 *.wav"
+               "oggenc -q 10 *.wav"
+               "oggdec *.ogg > /dev/null")
+benchmarkValidation=("wc -c 1.ogg | cut -d ' ' -f 1"
+                     "wc -c 1.ogg | cut -d ' ' -f 1"
+                     "wc -c 1.wav | cut -d ' ' -f 1")
+benchmarkRepetition=(2
+                     2
+                     3)
+benchmarkAnalyze=( "${benchmarkTest[@]}" )
+
+# Import functions for this benchmark
+. ${executionPath}/common/benchmark-timer.sh
+
+requireTools oggenc oggdec
