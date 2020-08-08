45cc4f3aa865a8e1f6e63e53299ba73bcff4abb3
Peter O'Connor <sunnyflunk@gmail.com>
2020-08-08 16:01:07 +1000

    Add an initial results generation
    This produces a simple table with a summarized mean and sd for each of the unique test results. Further work to make this useful on its own, or can use another tool to depict the output.

---
+++ generate-results.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+testName=${1}
+
+cat << EOF > /tmp/summarise.R
+# Import data results
+testName <- "$testName"
+dataResults <- read.csv('/home/crimson/BT-Results/$testName.csv', header = FALSE)
+colnames(dataResults) <- c("Label", "Distro", "Kernel", "Date", "Test", "Note", "Result", "Validation")
+
+Merged <- paste(dataResults\$Label, dataResults\$Distro, dataResults\$Kernel, dataResults\$Date, dataResults\$Test, dataResults\$Note)
+dataResults <- cbind(dataResults, Merged)
+
+dataUnique <- unique(dataResults\$Merged)
+for( index in 1:length(dataUnique) )
+{
+    tmp <- subset(dataResults, Merged == dataUnique[index])
+    tmpSubset <- tmp[1,c("Label", "Distro", "Kernel", "Date", "Note", "Test", "Merged")]
+    tmpSubset\$Mean <- mean(tmp\$Result)
+    tmpSubset\$SD <- sd(tmp\$Result)
+    if ( length(unique(tmp\$Validation)) == 1 ){
+        tmpSubset\$Valid <- unique(tmp\$Validation)
+    } else {
+        tmpSubset\$Valid <- NA
+    }
+
+    if( exists("dataCombined") ) {
+        dataCombined <- rbind(dataCombined, tmpSubset[,c("Label", "Distro", "Kernel", "Date", "Note", "Test", "Mean", "SD", "Valid")])
+    } else {
+        dataCombined <- tmpSubset[,c("Label", "Distro", "Kernel", "Date", "Note", "Test", "Mean", "SD", "Valid")]
+    }
+}
+
+write.csv(dataCombined, '/tmp/summarise.csv', row.names = FALSE)
+options(width=1000)
+print(dataCombined)
+EOF
+Rscript /tmp/summarise.R
+echo ""
+echo ""
+echo ""
+cat /tmp/summarise.csv
