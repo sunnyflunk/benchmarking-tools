3f5771c1b6e3557bedc8ea3c86ebaddafd0cfae1
Peter O'Connor <sunnyflunk@serpentos.com>
2020-09-11 12:43:43 +1000

    benchmarks: Add cmake
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

---
+++ benchmarks/cmake.sh
@@ -0,0 +1,20 @@
+#!/bin/true
+
+benchmarkSources=("llvm")
+benchmarkSetup=("cp ${BT_CACHE_DIR}/llvm-10.0.0.src.tar.xz ."
+                "tar xf llvm-10.0.0.src.tar.xz")
+benchmarkLabels=("Build LLVM")
+benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src"
+                  "rm -rf build"
+                  "mkdir build"
+                  "cd build")
+benchmarkTest=("cmake .. -DLLVM_TARGETS_TO_BUILD=host > $BT_RUNBENCHMARKS_DIR/build.log")
+benchmarkValidation=("echo '0'")
+benchmarkRepetition=(1)
+benchmarkAnalyze=( "${benchmarkTest[@]}" )
+
+# Import functions for this benchmark
+. ${executionPath}/common/benchmark-timer.sh
+. ${executionPath}/common/compiler.sh
+
+requireTools cmake make
