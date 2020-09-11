dd05bbd29065eb5a8c909e4da9adf38b7859a94c
Peter O'Connor <sunnyflunk@serpentos.com>
2020-09-11 16:25:15 +1000

    analyze: Label perf data by test and no longer delete
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- analyze-benchmark.sh
+++ analyze-benchmark.sh
@@ -18,16 +18,15 @@ for test in "${!benchmarkAnalyze[@]}"; do
 
     # Run analyze function
     rm -rf ${BT_RUNBENCHMARKS_DIR}/perf* || serpentFail "Failed to clean up temporary files"
-    eval perf record -z -F 15000 -o ${BT_RUNBENCHMARKS_DIR}/perf.data -- "${benchmarkAnalyze[$test]}" || serpentFail "Failed to record perf"
+    eval perf record -z -F 15000 -o ${BT_RUNBENCHMARKS_DIR}/perf-$test.data -- "${benchmarkAnalyze[$test]}" || serpentFail "Failed to record perf"
     [ ! -z "${benchmarkPosttest[0]}" ] && runCommands "${benchmarkPosttest[@]}"
     [ ! -z "${benchmarkPretest[0]}" ] && runCommands "${benchmarkPretest[@]}"
-    eval perf stat -B -e task-clock,cycles,instructions,L1-icache-misses,iTLB-load-misses,cache-references,cache-misses,branches,branch-misses,faults,context-switches,cpu-migrations -o ${BT_RUNBENCHMARKS_DIR}/perf -- "${benchmarkAnalyze[$test]}" || serpentFail "Failed to execute perf"
+    eval perf stat -B -e task-clock,cycles,instructions,L1-icache-misses,iTLB-load-misses,cache-references,cache-misses,branches,branch-misses,faults,alignment-faults,context-switches,cpu-migrations -o ${BT_RUNBENCHMARKS_DIR}/perf-$test -- "${benchmarkAnalyze[$test]}" || serpentFail "Failed to execute perf"
 
-    eval perf report --stdio --sort dso,sym --percent-limit 0.05 -i ${BT_RUNBENCHMARKS_DIR}/perf.data | grep -v "^#" > ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
+    eval perf report --stdio --sort dso,sym --percent-limit 0.05 -i ${BT_RUNBENCHMARKS_DIR}/perf-$test.data | grep -v "^#" > ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
     echo "" >> ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
-    eval perf report --stdio --sort dso -i ${BT_RUNBENCHMARKS_DIR}/perf.data | grep -v "^#" >> ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
+    eval perf report --stdio --sort dso -i ${BT_RUNBENCHMARKS_DIR}/perf-$test.data | grep -v "^#" >> ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
     echo "" >> ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
-    cat ${BT_RUNBENCHMARKS_DIR}/perf >> ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
-    rm -rf ${BT_RUNBENCHMARKS_DIR}/perf* || serpentFail "Failed to clean up temporary files"
+    cat ${BT_RUNBENCHMARKS_DIR}/perf-$test >> ${BT_RESULTS_DIR}/Perf-$testName-$testDate-$test
     [ ! -z "${benchmarkPosttest[0]}" ] && runCommands "${benchmarkPosttest[@]}"
 done
