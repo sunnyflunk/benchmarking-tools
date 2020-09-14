5d7587e8cdd46f96c46a5ab5851a81332071470f
Peter O'Connor <sunnyflunk@serpentos.com>
2020-09-14 17:35:13 +1000

    common: Make pre/post tests array based
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- common/benchmark-result.sh
+++ common/benchmark-result.sh
@@ -6,10 +6,10 @@ function runBenchmark()
     [ ! -z "${1}" ] || serpentFail "Incorrect use of runBenchmark"
     measuredTime=0
     for run in $(seq 1 1 "${benchmarkRepetition[$test]}"); do
-        [ ! -z "${benchmarkPretest[0]}" ] && runCommands "${benchmarkPretest[@]}"
+        [ ! -z "${benchmarkPretest[0]}" ] && runCommands "${benchmarkPretest[$test]}"
         stepTime=$(runCommands "${benchmarkTest[$test]}")
         measuredTime=$(awk "BEGIN {print $measuredTime+$stepTime; exit}")
-        [ ! -z "${benchmarkPosttest[0]}" ] && runCommands "${benchmarkPosttest[@]}"
+        [ ! -z "${benchmarkPosttest[0]}" ] && runCommands "${benchmarkPosttest[$test]}"
     done
     echo $measuredTime
 }
