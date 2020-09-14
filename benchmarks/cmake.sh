c9e3ad8fa066ffb7a6ea1a9982443c65d2420d9e
Peter O'Connor <sunnyflunk@serpentos.com>
2020-09-14 17:37:11 +1000

    benchmarks: Convert to single line for pretests
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- benchmarks/cmake.sh
+++ benchmarks/cmake.sh
@@ -4,10 +4,7 @@ benchmarkSources=("llvm")
 benchmarkSetup=("cp ${BT_CACHE_DIR}/llvm-10.0.0.src.tar.xz ."
                 "tar xf llvm-10.0.0.src.tar.xz")
 benchmarkLabels=("cmake LLVM")
-benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src"
-                  "rm -rf build"
-                  "mkdir build"
-                  "cd build")
+benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src && rm -rf build && mkdir build && cd build")
 benchmarkTest=("cmake .. -DLLVM_TARGETS_TO_BUILD=host > $BT_RUNBENCHMARKS_DIR/build.log")
 benchmarkValidation=("echo '0'")
 benchmarkRepetition=(1)
--- benchmarks/configure.sh
+++ benchmarks/configure.sh
@@ -4,10 +4,7 @@ benchmarkSources=("gettext")
 benchmarkSetup=("cp ${BT_CACHE_DIR}/gettext-0.21.tar.xz ."
                 "tar xf gettext-0.21.tar.xz")
 benchmarkLabels=("Configure gettext")
-benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/gettext-0.21"
-                  "rm -rf build"
-                  "mkdir build"
-                  "cd build")
+benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/gettext-0.21 && rm -rf build && mkdir build && cd build")
 benchmarkTest=("../configure > $BT_RUNBENCHMARKS_DIR/build.log")
 benchmarkValidation=("echo '0'")
 benchmarkRepetition=(1)
