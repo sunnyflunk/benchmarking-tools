c71bf8cba7c614a4960c6864fbf2d17b77f044fe
Peter O'Connor <sunnyflunk@serpentos.com>
2020-09-22 20:15:49 +1000

    benchmarks/compile-time-llvm: Stop including cmake output
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- benchmarks/compile-time-llvm.sh
+++ benchmarks/compile-time-llvm.sh
@@ -4,7 +4,7 @@ benchmarkSources=("llvm")
 benchmarkSetup=("cp ${BT_CACHE_DIR}/llvm-10.0.0.src.tar.xz ."
                 "tar xf llvm-10.0.0.src.tar.xz")
 benchmarkLabels=("Build LLVM")
-benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src && rm -rf build && mkdir build && cd build && cmake .. -DLLVM_TARGETS_TO_BUILD=host")
+benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src && rm -rf build && mkdir build && cd build && cmake .. -DLLVM_TARGETS_TO_BUILD=host > $BT_RUNBENCHMARKS_DIR/cmake.log")
 benchmarkTest=("make -j4 llvm-ar > $BT_RUNBENCHMARKS_DIR/build.log")
 benchmarkValidation=("wc -c $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src/build/bin/llvm-ar | cut -d ' ' -f 1")
 benchmarkRepetition=(1)
