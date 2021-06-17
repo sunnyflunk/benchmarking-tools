ae1fca073dfb2ce3093a3917e11e2fa8db7ce50b
Peter O'Connor <sunnyflunk@serpentos.com>
2021-06-17 12:17:46 +1000

    benchmarks: Ensure compiler is set before configuring cmake
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- benchmarks/cmake.sh
+++ benchmarks/cmake.sh
@@ -1,17 +1,17 @@
 #!/bin/true
 
+# Import functions for this benchmark
+. ${executionPath}/common/benchmark-timer.sh
+. ${executionPath}/common/compiler.sh
+
 benchmarkSources=("llvm")
 benchmarkSetup=("cp ${BT_CACHE_DIR}/llvm-10.0.0.src.tar.xz ."
                 "tar xf llvm-10.0.0.src.tar.xz")
 benchmarkLabels=("cmake LLVM")
 benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src && rm -rf build && mkdir build && cd build")
-benchmarkTest=("cmake .. -DLLVM_TARGETS_TO_BUILD=host > $BT_RUNBENCHMARKS_DIR/build.log")
+benchmarkTest=("cmake .. -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DLLVM_TARGETS_TO_BUILD=host > $BT_RUNBENCHMARKS_DIR/build.log")
 benchmarkValidation=("echo '0'")
 benchmarkRepetition=(1)
 benchmarkAnalyze=( "${benchmarkTest[@]}" )
-
-# Import functions for this benchmark
-. ${executionPath}/common/benchmark-timer.sh
-. ${executionPath}/common/compiler.sh
 
 requireTools cmake
--- benchmarks/compile-time-llvm.sh
+++ benchmarks/compile-time-llvm.sh
@@ -1,17 +1,17 @@
 #!/bin/true
 
+# Import functions for this benchmark
+. ${executionPath}/common/benchmark-timer.sh
+. ${executionPath}/common/compiler.sh
+
 benchmarkSources=("llvm")
 benchmarkSetup=("cp ${BT_CACHE_DIR}/llvm-10.0.0.src.tar.xz ."
                 "tar xf llvm-10.0.0.src.tar.xz")
 benchmarkLabels=("Build LLVM")
-benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src && rm -rf build && mkdir build && cd build && cmake .. -DLLVM_TARGETS_TO_BUILD=host > $BT_RUNBENCHMARKS_DIR/cmake.log")
+benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src && rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DLLVM_TARGETS_TO_BUILD=host > $BT_RUNBENCHMARKS_DIR/cmake.log")
 benchmarkTest=("make -j4 llvm-ar > $BT_RUNBENCHMARKS_DIR/build.log")
 benchmarkValidation=("wc -c $BT_RUNBENCHMARKS_DIR/llvm-10.0.0.src/build/bin/llvm-ar | cut -d ' ' -f 1")
 benchmarkRepetition=(1)
 benchmarkAnalyze=( "${benchmarkTest[@]}" )
-
-# Import functions for this benchmark
-. ${executionPath}/common/benchmark-timer.sh
-. ${executionPath}/common/compiler.sh
 
 requireTools cmake cut make wc
