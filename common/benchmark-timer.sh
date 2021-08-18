afc4c0b8db3ec8b70b94432a5eba9a164eb49211
Peter O'Connor <sunnyflunk@serpentos.com>
2021-08-19 00:30:19 +1000

    common/benchmark-timer: Use perf as timer and allow perf to run repeats if possible
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- common/benchmark-timer.sh
+++ common/benchmark-timer.sh
@@ -1,11 +1,15 @@
 #!/bin/true
 
 # Record the time taken for a command to run
-function recordTime () {
-    START=$(date +%s.%N)
-    eval "${1}"
-    FINISH=$(date +%s.%N)
-    echo $(awk "BEGIN {print $FINISH-$START; exit}")
+function recordTime() {
+    eval "perf stat -e task-clock,cycles,instructions,L1-icache-misses,cache-references,cache-misses,branches,branch-misses -o ${BT_RUNBENCHMARKS_DIR}/perf-$test -- ${1}"
+    echo $(grep "time elapsed" ${BT_RUNBENCHMARKS_DIR}/perf-$test | awk '{ print $1 }' )
+}
+
+# Record the time taken for each of x runs
+function recordTimeRepeat() {
+    eval "perf stat -e task-clock,cycles,instructions,L1-icache-misses,cache-references,cache-misses,branches,branch-misses -o ${BT_RUNBENCHMARKS_DIR}/perf-$test --repeat=${benchmarkRepetition[$test]} -- ${1}"
+    echo $(grep "time elapsed" ${BT_RUNBENCHMARKS_DIR}/perf-$test | awk '{ print $1 }' )
 }
 
 # Run and record benchmark information
@@ -14,10 +18,19 @@ function runBenchmark()
     [ ! -z "${1}" ] || serpentFail "Incorrect use of runBenchmark"
     measuredTime=0
     for run in $(seq 1 1 "${benchmarkRepetition[$test]}"); do
-        [ ! -z "${benchmarkPretest[0]}" ] && runCommands "${benchmarkPretest[$test]}"
+        [ ! -z "${benchmarkPretest[$test]}" ] && runCommands "${benchmarkPretest[$test]}"
         stepTime=$(recordTime "${benchmarkTest[$test]}")
         measuredTime=$(awk "BEGIN {print $measuredTime+$stepTime; exit}")
-        [ ! -z "${benchmarkPosttest[0]}" ] && runCommands "${benchmarkPosttest[$test]}"
+        [ ! -z "${benchmarkPosttest[$test]}" ] && runCommands "${benchmarkPosttest[$test]}"
     done
-    echo $measuredTime
+    echo $(awk "BEGIN {print $measuredTime/${benchmarkRepetition[$test]}; exit}")
 }
+
+# Run and record benchmark information
+function runBenchmarkRepeat()
+{
+    [ ! -z "${1}" ] || serpentFail "Incorrect use of runBenchmark"
+    echo $(recordTimeRepeat "${benchmarkTest[$test]}")
+}
+
+requireTools awk grep perf seq
