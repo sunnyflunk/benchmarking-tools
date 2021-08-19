09baa71fa7bae352c51a7690c8b2024d8c5fe33a
Peter O'Connor <sunnyflunk@serpentos.com>
2021-08-19 11:48:05 +1000

    benchmarks/configure: Put into a script so perf can track it
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- benchmarks/configure.sh
+++ benchmarks/configure.sh
@@ -6,10 +6,11 @@
 
 benchmarkSources=("gettext")
 benchmarkSetup=("cp ${BT_CACHE_DIR}/gettext-0.21.tar.xz ."
-                "tar xf gettext-0.21.tar.xz")
+                "tar xf gettext-0.21.tar.xz"
+                "echo \"CC=${CC} CXX=${CXX} ../configure > $BT_RUNBENCHMARKS_DIR/build.log\" > gettext-0.21/configure.sh")
 benchmarkLabels=("Configure gettext")
 benchmarkPretest=("cd $BT_RUNBENCHMARKS_DIR/gettext-0.21 && rm -rf build && mkdir build && cd build")
-benchmarkTest=("CC=${CC} CXX=${CXX} ../configure > $BT_RUNBENCHMARKS_DIR/build.log")
+benchmarkTest=("sh ../configure.sh")
 benchmarkValidation=("echo '0'")
 benchmarkRepetition=(1)
 benchmarkAnalyze=( "${benchmarkTest[@]}" )
