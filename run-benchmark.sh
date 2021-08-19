a7b3d52696ed275519ec6b4107e8197f26133982
Peter O'Connor <sunnyflunk@serpentos.com>
2021-08-19 11:09:09 +1000

    run-benchmark: Point data collection to right perf file
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -27,13 +27,13 @@ for run in $(seq 1 1 ${benchmarkRuns}); do
         fi
         testValidation=$(runCommands "${benchmarkValidation[$test]}")
 
-        testInstructions=$(grep " instructions.u" perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
-        testCycles=$(grep " cycles.u" perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
-        testL1CacheMisses=$(grep " L1-icache-misses.u" perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
-        testCacheReferences=$(grep " cache-references.u" perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
-        testCacheMisses=$(grep " cache-misses.u" perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
-        testBranches=$(grep " branches.u" perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
-        testBranchMisses=$(grep " branch-misses.u" perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
+        testInstructions=$(grep " instructions.u" ${BT_RUNBENCHMARKS_DIR}/perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
+        testCycles=$(grep " cycles.u" ${BT_RUNBENCHMARKS_DIR}/perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
+        testL1CacheMisses=$(grep " L1-icache-misses.u" ${BT_RUNBENCHMARKS_DIR}/perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
+        testCacheReferences=$(grep " cache-references.u" ${BT_RUNBENCHMARKS_DIR}/perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
+        testCacheMisses=$(grep " cache-misses.u" ${BT_RUNBENCHMARKS_DIR}/perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
+        testBranches=$(grep " branches.u" ${BT_RUNBENCHMARKS_DIR}/perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
+        testBranchMisses=$(grep " branch-misses.u" ${BT_RUNBENCHMARKS_DIR}/perf-$test | grep -v "not counted" | awk '{ print $1 }' | sed 's/,//g')
 
         # Record results
         echo "$testLabel,$testDistro,$testKernel,$testDate,${benchmarkLabels[$test]},$benchmarkNote,$testResult,$testValidation,$testInstructions,$testCycles,$testL1CacheMisses,$testCacheReferences,$testCacheMisses,$testBranches,$testBranchMisses" >> ${BT_RESULTS_DIR}/$testName.csv
