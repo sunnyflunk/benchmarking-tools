45aa0f791f9a4140407aa0abe1fbb8170b3dfee8
Cameron Nemo <cam@nohom.org>
2023-01-16 13:30:09 -0800

    benchmarks: add opus

---
+++ benchmarks/opus.sh
@@ -0,0 +1,21 @@
+#!/bin/true
+
+. ${executionPath}/benchmarks/helpers/wavfile.sh
+benchmarkLabels=("Encode (256 kbit/s)"
+                 "Encode (128 kbit/s)"
+                 "Decode")
+benchmarkTest=("opusenc --bitrate 256 1.wav 1.opus"
+               "opusenc --bitrate 128 1.wav 1.opus"
+               "opusdec 1.opus > /dev/null")
+benchmarkValidation=("wc -c 1.opus | cut -d ' ' -f 1"
+                     "wc -c 1.opus | cut -d ' ' -f 1"
+                     "wc -c 1.wav | cut -d ' ' -f 1")
+benchmarkRepetition=(2
+                     2
+                     3)
+benchmarkAnalyze=( "${benchmarkTest[@]}" )
+
+# Import functions for this benchmark
+. ${executionPath}/common/benchmark-timer.sh
+
+requireTools cut opusenc opusdec wc
