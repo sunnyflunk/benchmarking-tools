b9e7f33cb1917fef81cc137cee1afecc723e0695
Peter O'Connor <sunnyflunk@serpentos.com>
2023-01-25 22:35:00 +1100

    benchmarks: Tweak opus test
    Write wavfile to different name.

--- benchmarks/opus.sh
+++ benchmarks/opus.sh
@@ -6,13 +6,13 @@ benchmarkLabels=("Encode (256 kbit/s)"
                  "Decode")
 benchmarkTest=("opusenc --bitrate 256 1.wav 1.opus"
                "opusenc --bitrate 128 1.wav 1.opus"
-               "opusdec 1.opus > /dev/null")
+               "opusdec 1.opus 2.wav")
 benchmarkValidation=("wc -c 1.opus | cut -d ' ' -f 1"
                      "wc -c 1.opus | cut -d ' ' -f 1"
-                     "wc -c 1.wav | cut -d ' ' -f 1")
-benchmarkRepetition=(2
-                     2
-                     3)
+                     "wc -c 2.wav | cut -d ' ' -f 1")
+benchmarkRepetition=(1
+                     1
+                     2)
 benchmarkAnalyze=( "${benchmarkTest[@]}" )
 
 # Import functions for this benchmark
