ff1f36ad5fcbb2556178b6a4a8d6f8e06dbe55eb
Peter O'Connor <sunnyflunk@serpentos.com>
2023-01-30 19:45:40 +1100

    power-benchmark: Overhaul the process to collect both power data and results
    Now results can be generated from a single run.
    
    Other improvements:
    - Do 4 runs of each test now that it's shorter
    - Short benchmarks are now run multiple times and averaged (like the normal results test)
    - Removed a 2s sleep since we added a 10s one already
    
    Needs to be tested on a machine without full RAPL data, but should still report 0.

--- common/benchmark.sh
+++ common/benchmark.sh
@@ -5,6 +5,11 @@ function runBenchmark()
 {
     [ ! -z "${1}" ] || serpentFail "Incorrect use of runBenchmark"
     measuredResult=0
+    measuredPsys=0
+    measuredPkg=0
+    measuredCores=0
+    measuredGPU=0
+    measuredRam=0
     for run in $(seq 1 1 "${benchmarkRepetition[$test]}"); do
         [ ! -z "${benchmarkPretest[$test]}" ] && runCommands "${benchmarkPretest[$test]}"
         if [ -z ${DATA_BENCHMARK} ]; then
@@ -14,9 +19,32 @@ function runBenchmark()
             stepResult=$(runCommands "${perfCommand} -o ${BT_RUNBENCHMARKS_DIR}/perf-$test -- ${benchmarkTest[$test]}")
         fi
         measuredResult=$(awk "BEGIN {print $measuredResult+$stepResult; exit}")
+
+        if [ ! -z ${POWER_BENCHMARK} ]; then
+            stepPsys=$(grep "/energy-psys" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+            stepPkg=$(grep "/energy-pkg" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+            stepCores=$(grep "/energy-cores" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+            stepGPU=$(grep "/energy-gpu" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+            stepRam=$(grep "/energy-ram" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+
+            measuredPsys=$(awk "BEGIN {print $measuredPsys+$stepPsys; exit}")
+            measuredPkg=$(awk "BEGIN {print $measuredPkg+$stepPkg; exit}")
+            measuredCores=$(awk "BEGIN {print $measuredCores+$stepCores; exit}")
+            measuredGPU=$(awk "BEGIN {print $measuredGPU+$stepGPU; exit}")
+            measuredRam=$(awk "BEGIN {print $measuredRam+$stepRam; exit}")
+        fi
         [ ! -z "${benchmarkPosttest[$test]}" ] && runCommands "${benchmarkPosttest[$test]}"
     done
-    echo $(awk "BEGIN {print $measuredResult/${benchmarkRepetition[$test]}; exit}")
+
+    avgResult=$(awk "BEGIN {print $measuredResult/${benchmarkRepetition[$test]}; exit}")
+    if [ ! -z ${POWER_BENCHMARK} ]; then
+        avgPsys=$(awk "BEGIN {print $measuredPsys/${benchmarkRepetition[$test]}; exit}")
+        avgPkg=$(awk "BEGIN {print $measuredPkg/${benchmarkRepetition[$test]}; exit}")
+        avgCores=$(awk "BEGIN {print $measuredCores/${benchmarkRepetition[$test]}; exit}")
+        avgGPU=$(awk "BEGIN {print $measuredGPU/${benchmarkRepetition[$test]}; exit}")
+        avgRam=$(awk "BEGIN {print $measuredRam/${benchmarkRepetition[$test]}; exit}")
+    fi
+    echo $avgResult $avgPsys $avgPkg $avgCores $avgGPU $avgRam
 }
 
 # Run and record benchmark information
@@ -24,7 +52,16 @@ function runBenchmarkRepeat()
 {
     [ ! -z "${1}" ] || serpentFail "Incorrect use of runBenchmark"
     eval "${perfCommand} -o ${BT_RUNBENCHMARKS_DIR}/perf-$test --repeat=${benchmarkRepetition[$test]} -- ${benchmarkTest[$test]} > /dev/null 2>&1"
-    echo $(grep "time elapsed" ${BT_RUNBENCHMARKS_DIR}/perf-$test | awk '{ print $1 }')
+    avgResult=$(grep "time elapsed" ${BT_RUNBENCHMARKS_DIR}/perf-$test | awk '{ print $1 }')
+    if [ ! -z ${POWER_BENCHMARK} ]; then
+        avgPsys=$(grep "/energy-psys" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+        avgPkg=$(grep "/energy-pkg" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+        avgCores=$(grep "/energy-cores" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+        avgGPU=$(grep "/energy-gpu" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+        avgRam=$(grep "/energy-ram" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+    fi
+    echo $avgResult $avgPsys $avgPkg $avgCores $avgGPU $avgRam
+
 }
 
 requireTools awk grep perf seq
--- run-power-benchmark.sh
+++ run-power-benchmark.sh
@@ -4,9 +4,11 @@ testName=${1}
 testLabel=${2}
 executionPath=$(dirname $(realpath -s $0))
 # Default number of times to run the tests
-benchmarkRuns=3
+benchmarkRuns=4
 kernelPerf=$(cat /proc/sys/kernel/perf_event_paranoid)
 perfCommand="perf stat -e power/energy-psys/,power/energy-pkg/,power/energy-cores/,power/energy-gpu/,power/energy-ram/"
+# Make benchmark function also report the power average power data
+POWER_BENCHMARK=1
 
 if [ ${kernelPerf} -ge 1 ];
 then
@@ -31,28 +33,28 @@ printInfo "Preparing benchmark environment"
 . ${executionPath}/common/prepare-benchmark.sh
 . ${executionPath}/common/benchmark.sh
 
-# Override some values, we need timer function for per /s W values and force one repetition as that's all that's measured
-benchmarkRepetition=(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)
-
 for run in $(seq 1 1 ${benchmarkRuns}); do
     printInfo "Begin iteration $run of ${benchmarkRuns}"
     for test in "${!benchmarkTest[@]}"; do
         # Wait to settle system after previous test
         sleep 10
+
         # If possible run the repeats through perf
-        testResult=$(runBenchmark "${benchmarkTest[$test]}")
+        if [ -z "${benchmarkPretest[$test]}" ] && [ -z "${benchmarkPosttest[$test]}" ] && [ -z "${DATA_BENCHMARK}" ]; then
+            tmpResult=$(runBenchmarkRepeat "${benchmarkTest[$test]}")
+        else
+            tmpResult=$(runBenchmark "${benchmarkTest[$test]}")
+        fi
+        testResult=$(echo ${tmpResult} | cut -f1 -d' ')
         testValidation=$(runCommands "${benchmarkValidation[$test]}")
 
-        testPsys=$(grep "/energy-psys" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
-        testPkg=$(grep "/energy-pkg" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
-        testCores=$(grep "/energy-cores" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
-        testGPU=$(grep "/energy-gpu" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
-        testRam=$(grep "/energy-ram" ${BT_RUNBENCHMARKS_DIR}/perf-$test | sed 's|<not|0|' | awk '{ print $1 }' | sed 's/,//g')
+        testPsys=$(echo ${tmpResult} | cut -f2 -d' ')
+        testPkg=$(echo ${tmpResult} | cut -f3 -d' ')
+        testCores=$(echo ${tmpResult} | cut -f4 -d' ')
+        testGPU=$(echo ${tmpResult} | cut -f5 -d' ')
+        testRam=$(echo ${tmpResult} | cut -f6 -d' ')
 
         # Record results
         echo "$testName,$testLabel,$testDistro,$testKernel,$testDate,${benchmarkLabels[$test]},$testResult,$testValidation,$testPsys,$testPkg,$testCores,$testGPU,$testRam" >> ${BT_RESULTS_DIR}/results-power.csv
-
-        # Sleep to let the system go back to normal
-        sleep 2
     done
 done
